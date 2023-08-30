76a126c07ff8b47e19e923abee1ae505d71f39fb
Juan Jose Ceballos Navia <juanfix@gmail.com>
2023-08-30 15:13:33 -0500

    index.js modified to deploy on Render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import app from './app';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const main = () => {
+  require('dotenv').config();
+
+  // Crear servidor de express
+  const app = express();
+
+  // CORS
+  app.use(cors());
+
+  // Directorio publico
+  app.use(express.static('public'));
+
+  // Middlewares
+  app.use(morgan('dev'));
+
+  // Lectura y parseo del body
+  app.use(express.json());
+
+  // Routes
+  app.use('/api/mail', require('./routes/mail.routes'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + 'public/index.html');
+  });
+
+  // Set port
+  app.set('port', process.env.PORT);
+
   app.listen(app.get('port'), () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
